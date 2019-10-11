750bde7902489e2fd9705263c68bd0960d593957
Kartik <kaaro@Apples-MacBook-Pro.local>
2019-10-11 14:29:03 +0530

    kaaroStream update

--- jumper/kaaro_stream.js
+++ jumper/kaaro_stream.js
@@ -1,50 +1,81 @@
-var canvas = document.querySelector("canvas");
+// var canvas = document.querySelector("canvas");
 
-// Optional frames per second argument.
-var stream = canvas.captureStream(25);
-var recordedChunks = [];
+// // Optional frames per second argument.
+// var stream = canvas.captureStream(25);
+// var recordedChunks = [];
 
-console.log(stream);
-var options = { mimeType: "video/webm; codecs=vp9" };
-mediaRecorder = new MediaRecorder(stream);
+// console.log(stream);
+// var options = { mimeType: "video/webm; codecs=vp9" };
+// mediaRecorder = new MediaRecorder(stream);
 
-mediaRecorder.ondataavailable = handleDataAvailable;
-mediaRecorder.start();
+// mediaRecorder.ondataavailable = handleDataAvailable;
+// mediaRecorder.start();
 
-function handleDataAvailable(event) {
-  console.log("data-available");
-  if (event.data.size > 0) {
-    recordedChunks.push(event.data);
-    console.log(recordedChunks);
-    download();
+// function handleDataAvailable(event) {
+//   console.log("data-available");
+//   if (event.data.size > 0) {
+//     recordedChunks.push(event.data);
+//     console.log(recordedChunks);
+//     download();
 
-  } else {
-    // ...
-  }
-}
-function download() {
-    var blob = new Blob(recordedChunks, {
-      type: 'video/webm'
-    });
-    var url = URL.createObjectURL(blob);
-    var a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style = 'display: none';
-    a.href = url;
-    a.download = 'test.webm';
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }
+//   } else {
+//     // ...
+//   }
+// }
+// function download() {
+//   var blob = new Blob(recordedChunks, {
+//     type: 'video/webm'
+//   });
+//   var url = URL.createObjectURL(blob);
+//   var a = document.createElement('a');
+//   document.body.appendChild(a);
+//   a.style = 'display: none';
+//   a.href = url;
+//   a.download = 'test.webm';
+//   a.click();
+//   window.URL.revokeObjectURL(url);
+// }
 
-//for logging status
-setInterval( (event) => {
-    
-    console.log(mediaRecorder);
+// //for logging status
+// setInterval((event) => {
 
-},1500);
+//   console.log(mediaRecorder);
 
-// demo: to download after 9sec
-setTimeout( (event) => {
-    console.log("stopping");
-    mediaRecorder.stop();
-}, 15000);
+// }, 1500);
+
+// // demo: to download after 9sec
+// setTimeout((event) => {
+//   console.log("stopping");
+//   mediaRecorder.stop();
+// }, 15000);
+
+
+
+let kaaro_stream_bridge_url = "boomboom.store:3012";
+let stream_key = '--';
+
+let mediaRecorder;
+let mediaStream;
+let ws_url = window.location.protocol.replace('http', 'ws') + '//' + // http: => ws:, https: -> wss:
+  kaaro_stream_bridge_url +
+  '/rtmp/' +
+  encodeURIComponent(`rtmp://live-sin.twitch.tv/app/${stream_key}`);
+console.log(ws_url);
+const ws = new WebSocket(ws_url);
+ws.addEventListener('open', (e) => {
+  console.log('WebSocket Open', e);
+  mediaStream = document.querySelector('canvas').captureStream(30); // 30 FPS
+  mediaRecorder = new MediaRecorder(mediaStream, {
+    mimeType: 'video/webm;codecs=h264',
+    videoBitsPerSecond: 3000000
+  });
+  mediaRecorder.addEventListener('dataavailable', (e) => {
+    ws.send(e.data);
+  });
+  mediaRecorder.addEventListener('stop', ws.close.bind(ws));
+  mediaRecorder.start(1000); // Start recording, and dump data every second
+});
+ws.addEventListener('close', (e) => {
+  console.log('WebSocket Close', e);
+  mediaRecorder.stop();
+});
